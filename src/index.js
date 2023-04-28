2ec59fc0b1262235e4fd860ed0984cbf656b439a
olayinka06-dev <olayinkaconsult06@gmail.com>
2023-04-28 03:57:17 -0700

    changes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import App from './App';
-// import './index.css';
 import  './App.css';
-import Last from './Components/Last';
 import Final3 from './Components/Final3';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
